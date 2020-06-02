7dd35e971a2ff999198b9bac69b90e3a2f777052
unknown <athula1wasala@gmail.com>
2020-06-03 01:59:48 +0530

    update

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(["prefix" => "backend"], function () {
+
+
+    Route::get('/', function () {
+        return view('welcome');
+    })
+
+});;
