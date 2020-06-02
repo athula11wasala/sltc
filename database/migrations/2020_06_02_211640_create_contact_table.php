615222f17497df5131e5c5a427690b4663305585
unknown <athula1wasala@gmail.com>
2020-06-03 02:51:10 +0530

    modify route and added home_edit page

---
+++ database/migrations/2020_06_02_211640_create_contact_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_contact_info', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string ( 'description',512 );
+            $table->tinyInteger ( 'type' )->nullable ()->default ( 1 )->comment = "type 1=tel, 2=email 3=address";
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_contact_info');
+    }
+}
